eb7e559f062f6c9ba751465f128b9d5fd299564e
Luis Dietl <interruptedThread@gmail.com>
2024-08-10 00:14:02 +0200

    changes

--- code/loader.cpp
+++ code/loader.cpp
@@ -11,16 +11,18 @@ Level LoadFromFile(u32 stage, u32 *buffer)
     Level level = {};
     level.tiles = buffer;
 
-    // char path[1024];
-    // sprintf(path, "assets/levels/1part1.png");
-    // sprintf(path, "C:/Users/eario/Desktop/sgj24/sgj-24/assets/levels/1part1.png");
+    char path[1024];
+    sprintf(path, "assets/levels/test_level.png");
 
     i32 width;
     i32 height;
     i32 channel = 3;
 
-    u8* tmp = stbi_load("assets/levels/test_level.png", &width, &height, &channel, 0);
+    u8* tmp = stbi_load(path, &width, &height, &channel, 0);
     assert(tmp);
+
+    level.width = width;
+    level.height = height;
 
     u8* curr = tmp;
     for (u32 y = 0; y < level.height; ++y) {
