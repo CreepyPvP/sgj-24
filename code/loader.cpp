717c5018c619f09c70ed0c20c995335786d31177
MyrenEario <31888568+MyrenEario@users.noreply.github.com>
2024-08-10 10:22:38 +0200

    Level loader can now load walls.

--- code/loader.cpp
+++ code/loader.cpp
@@ -18,19 +18,30 @@ Level LoadFromFile(u32 stage, u32 *buffer)
     i32 height;
     i32 channel = 3;
 
-    u8* tmp = stbi_load(path, &width, &height, &channel, 0);
+    u8* tmp = stbi_load(path, &width, &height, &channel, STBI_rgb);
     assert(tmp);
 
     level.width = width;
     level.height = height;
 
+    printf("%d\n", level.width);
+    printf("%d\n", level.height);
+
     u8* curr = tmp;
     for (u32 y = 0; y < level.height; ++y) {
         for (u32 x = 0; x < level.width; ++x) {
-            if(curr[0] == 0 &&
+
+            if(curr[0] == 255 &&
+               curr[1] == 255 &&
+               curr[2] == 255){
+                level.tiles[x + y * level.width] = 0;
+            }
+            else if(curr[0] == 0 &&
                curr[1] == 0 &&
                curr[2] == 0){
                 level.tiles[x + y * level.width] = 1;
+            } else {
+                level.tiles[x + y * level.width] = 0;
             }
             curr += 3;
         }
